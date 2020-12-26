85ba727492aecc4f925c864949f193e68f0b944a
PzOng <pangszee.ong@gmail.com>
2020-12-27 00:19:42 +1030

    local env

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,38 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
 
+// const app = firebase.initializeApp({
+//   apiKey: "AIzaSyDlHKeYYkaP3Ebm6piVFdV7uP0aICc6DdQ",
+//   authDomain: "cheers-d14a9.firebaseapp.com",
+//   projectId: "cheers-d14a9",
+//   storageBucket: "cheers-d14a9.appspot.com",
+//   messagingSenderId: "549518657696",
+//   appId: "1:549518657696:web:873057997ed306ea5a61c5",
+//   measurementId: "G-VPXQSXE845"
+// })
+
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDlHKeYYkaP3Ebm6piVFdV7uP0aICc6DdQ",
-  authDomain: "cheers-d14a9.firebaseapp.com",
-  projectId: "cheers-d14a9",
-  storageBucket: "cheers-d14a9.appspot.com",
-  messagingSenderId: "549518657696",
-  appId: "1:549518657696:web:873057997ed306ea5a61c5",
-  measurementId: "G-VPXQSXE845"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGESENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  
 })
 
 export const db = app.firestore()
 export const auth = app.auth()
 export default app
+
+
+
+
+
+
+
+
+
+
+
